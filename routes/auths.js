82345294e55c2be6f2643e33931738affbbb73d0
Matthew Torres <matthew.torres211@gmail.com>
2021-01-10 23:36:54 +0800

    Initialize middleware to send email

--- routes/auths.js
+++ routes/auths.js
@@ -1,10 +1,11 @@
 import express from 'express'
+import sendMail from '../middlewares/sendMail'
 import User from '../models/User'
 import gateway from '../utils/gateway'
 
 const router = express.Router()
 
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
   const {
     email, firstName, lastName, password,
   } = req.body
@@ -13,14 +14,15 @@ router.post('/register', async (req, res) => {
     const { id } = await gateway.post('/consumers', { username: email })
     await gateway.post(`/consumers/${email}/basic-auth`, { password, username: email })
 
-    const newUser = await User.create({ firstName, lastName, consumerId: id })
+    const newUser = await (await User.create({ firstName, lastName, consumerId: id }))
     res.json(newUser.toJSON())
+    next()
   } catch (err) {
     console.log('err', err?.response)
     console.error(`Error creating user: ${err}`)
     res.status(err?.response?.status)
     res.send(err?.response?.data)
   }
-})
+}, sendMail)
 
 module.exports = router
