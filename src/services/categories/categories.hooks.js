03e4df2e07bd4d71c2db1235b2eb1b07b68004f4
Matthew Torres <matthew.torres211@gmail.com>
2021-03-15 22:58:47 +0800

    Add hooks when creating categories

--- src/services/categories/categories.hooks.js
+++ src/services/categories/categories.hooks.js
@@ -5,12 +5,33 @@ const unsetRaw = (context) => {
   return context
 }
 
+const trimData = (context) => {
+  const trimmedData = Object.keys(context.data).reduce((acc, curr) => {
+    if (typeof context.data[curr] === 'string') {
+      acc[curr] = context.data[curr].trim()
+    } else {
+      acc[curr] = context.data[curr]
+    }
+
+    return acc
+  }, {})
+
+  Object.assign(context.data, trimmedData)
+
+  return context
+}
+
+const appendUserId = (context) => {
+  Object.assign(context.data, { userId: context.params.user.id })
+  return context
+}
+
 module.exports = {
   before: {
     all: [authenticate('jwt')],
     find: [unsetRaw],
     get: [],
-    create: [],
+    create: [trimData, appendUserId],
     update: [],
     patch: [],
     remove: [],
