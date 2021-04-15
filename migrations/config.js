4b3e3db14f03936f0185f9c1e71cad70095aecd9
Matthew Torres <matthew.torres211@gmail.com>
2021-04-15 16:37:57 +0800

    Fix config

--- migrations/config.js
+++ migrations/config.js
@@ -2,12 +2,15 @@ const app = require('../src/app')
 
 const env = process.env.NODE_ENV || 'development'
 const dialect = 'postgres'
+console.log("app.get('sslMode')", app.get('sslMode'))
 
 module.exports = {
   [env]: {
     dialect,
+    dialectOptions: {
+      ssl: app.get('sslMode'),
+    },
     url: app.get(dialect),
-    ssl: app.get('sslMode'),
     migrationStorageTableName: '_migrations',
   },
 }
