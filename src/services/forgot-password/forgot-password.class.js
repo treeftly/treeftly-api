21af9fca5e58c77c30f04a67fe5ad1f3f792c254
Matthew Torres <matthew.torres211@gmail.com>
2021-04-22 17:11:57 +0800

    Add route to change password along with token verification

--- src/services/forgot-password/forgot-password.class.js
+++ src/services/forgot-password/forgot-password.class.js
@@ -1,27 +1,72 @@
-const { NotFound } = require('@feathersjs/errors')
+const { NotFound, BadRequest } = require('@feathersjs/errors')
+const differenceInHours = require('date-fns/differenceInHours')
 const crypto = require('crypto')
 
-const generateResetCode = (user) => {
-  // create ISO String
-  const now = new Date()
-
-  // Convert to Base64
-  const timeHash = Buffer.from(now.toISOString()).toString('base64')
-
-  // User string
-  const userString = `${user.id}${user.email}${user.password}${user.updatedAt}`
-  const userHash = crypto
-    .createHash('md5')
-    .update(userString)
-    .digest('hex')
-
-  return `${timeHash}-${userHash}`
-}
+const HOURS_DIFF = 24
 
 exports.ForgotPassword = class ForgotPassword {
   constructor(options, app) {
     this.options = options || {}
     this.app = app
+  }
+
+  userString(user) {
+    return `${user.id}${user.email}${user.password}${user.updatedAt}`
+  }
+
+  userHash(userString) {
+    return crypto
+      .createHash('md5')
+      .update(userString)
+      .digest('hex')
+  }
+
+  // Taken from https://www.shanestillwell.com/2018/11/24/password-reset-token-expiration/
+  generateResetCode(user) {
+    // create ISO String
+    const now = new Date()
+
+    // Convert to Base64
+    const timeHash = Buffer.from(now.toISOString()).toString('base64')
+
+    // User string
+    const userString = this.userString(user)
+    const userHash = this.userHash(userString)
+    const encodedUserId = Buffer.from(String(user.id), 'utf-8').toString('base64')
+
+    return `${timeHash}-${userHash}-${encodedUserId}`
+  }
+
+  async patch(id, data, params) {
+    const { password, confirmPassword } = data
+    const { token } = params.query
+    const userService = this.app.service('users')
+
+    const [timeHash, reqUserHash, encodedUserId] = token.split('-')
+    const userId = Buffer.from(encodedUserId, 'base64').toString('utf-8')
+
+    const timestamp = Buffer.from(timeHash, 'base64').toString('ascii')
+
+    const diff = differenceInHours(new Date(timestamp), new Date())
+
+    if (Math.abs(diff) > HOURS_DIFF) {
+      throw new BadRequest('Token has expired')
+    }
+
+    const user = await userService.get(userId)
+
+    const userString = this.userString(user)
+    const userHash = this.userHash(userString)
+
+    if (reqUserHash !== userHash) {
+      throw new BadRequest('Invalid token')
+    }
+
+    if (password !== confirmPassword) {
+      throw new BadRequest('Passwords does not match')
+    }
+
+    return userService.patch(user.id, { password })
   }
 
   async create(data) {
@@ -33,7 +78,7 @@ exports.ForgotPassword = class ForgotPassword {
       const [userData] = userResponse.data
 
       return {
-        token: generateResetCode(userData),
+        token: this.generateResetCode(userData),
         ...userData,
       }
     }
